d00b0d60099e36573fbe079f0f3f804feb96810b
dulcetiseduce1 <mariusdulce@tuta.io>
2022-07-05 16:08:08 +0200

    testimonials

--- src/components/TheTestimonialsQuote.vue
+++ src/components/TheTestimonialsQuote.vue
@@ -1,12 +1,78 @@
 <template>
-  <div class="request">
-    <div class="request-wrapper">
-      <div class="request-text">
-        <h2>Request A Free Quote</h2>
-        <P
-          >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</P
-        >
-        <a class="button" href="#">CLICK HERE</a>
+  <div>
+    <div class="testimonials">
+      <div class="testimonials-wrapper">
+        <div class="testimonials-text">
+          <span>HAPPY CUSTUMERS</span>
+          <h1>Testimonials</h1>
+        </div>
+
+        <div class="cards-wrapper">
+          <div class="testimonials-card">
+            <img
+              src="../assets/img/avada-movers-johndoe-final-400x400.jpg"
+              alt="john doe"
+            />
+            <div class="texts">
+              <h3>John Doe</h3>
+              <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat."
+              </p>
+            </div>
+          </div>
+          <div class="testimonials-card">
+            <img
+              src="../assets/img/avada-movers-janedoe-final-400x400.jpg"
+              alt="jane doe"
+            />
+            <div class="texts">
+              <h3>Jane Doe</h3>
+              <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat."
+              </p>
+            </div>
+          </div>
+          <div class="testimonials-card">
+            <img
+              src="../assets/img/avada-movers-johnsmith-final-400x400.jpg"
+              alt="john smith"
+            />
+            <div class="texts">
+              <h3>John Smith</h3>
+              <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat."
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="button-wrapper">
+          <a class="button" href="#">READ MORE TESTIMONIALS</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="request">
+      <div class="request-wrapper">
+        <div class="request-text">
+          <h2>Request A Free Quote</h2>
+          <P
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.</P
+          >
+          <a class="button" href="#">CLICK HERE</a>
+        </div>
       </div>
     </div>
   </div>
@@ -19,6 +85,98 @@ export default {
 </script>
 
 <style lang="scss">
+//testimonials
+.testimonials {
+  width: 100%;
+  height: 800px;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  background-image: url("../assets/img/avada-movers-testimonial-background.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-bottom: 100px;
+
+  .testimonials-wrapper {
+    width: 80%;
+    height: 80%;
+    margin: 0 auto;
+    padding-top: 30px;
+
+    .testimonials-text {
+      text-align: center;
+      margin-bottom: 10px;
+
+      span {
+        font-size: 13px;
+        color: rgb(80, 80, 80);
+        font-weight: 600;
+      }
+
+      h1 {
+        color: #6abf17;
+        margin-top: 10px;
+      }
+
+      p {
+        width: 40%;
+        margin: 0 auto;
+        font-size: 14px;
+        line-height: 25px;
+        margin-bottom: 25px;
+      }
+    }
+
+    .cards-wrapper {
+      width: 70%;
+      min-width: 950px;
+      height: calc(100% - 100px);
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      margin: 0 auto;
+
+      .testimonials-card {
+        width: 350px;
+        height: 490px;
+        padding: 15px;
+        text-align: center;
+
+        img {
+          width: 50%;
+          margin-bottom: 10px;
+          border-radius: 50%;
+        }
+
+        .texts {
+          padding: 20px 20px 25px 20px;
+
+          h3 {
+            color: #1775bf;
+            margin-bottom: 30px;
+          }
+
+          p {
+            font-size: 14px;
+            line-height: 25px;
+            margin-bottom: 20px;
+            font-style: italic;
+          }
+        }
+      }
+    }
+
+    .button-wrapper {
+      width: 300px;
+      margin: 0 auto;
+      text-align: center;
+
+      // button
+      @import "../assets/TheCss.scss";
+    }
+  }
+}
+
 .request {
   width: 100%;
   height: 510px;
@@ -45,7 +203,6 @@ export default {
 
     h2 {
       color: #6abf17;
-      font-size: 30px;
       margin: 15px 0;
     }
 
